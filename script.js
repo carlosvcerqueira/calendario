afe0f0537f5d921b7e1334a25b07420fb061c244
Carlos Vinicius <carlosvcerqueira@gmail.com>
2022-07-26 22:40:58 -0300

    feat: style e func

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ React.createElement("nav", {className: "calendar--nav"},
 React.createElement("a",{ onClick: () => changeMonth(date.month() - 1)}, "\u2039"),
 React.createElement("h1", {onClick: () => resetDate() }, date.format('MMM'), " ",
 React.createElement("small", null, date.format('YYYY'))),
-React.createElement("a", {onClick: () => changeMonth(date.month() + 1)}, "u203A"));
+React.createElement("a", {onClick: () => changeMonth(date.month() + 1)}, "\u203A"));
 
 const Day = ({currentDate, date, startDate, endDate, onClick}) => {
     let className = [];
@@ -43,4 +43,94 @@ const Days = ({date, startDate, endDate, onClick }) => {
     const nextsMonth = moment(date).add(1, 'month');
     let days = [];
     let labels= [];
-}
+
+    for (let i = 1; i <= 7; i++) {
+        labels.push(React.createElement("span", {className: "label"},
+        moment().day(i).format('ddd')));
+    }
+
+    for (let i = firstDayDate.day(); i > 1; i--) {
+        days.push(
+        React.createElement(Day, {key: moment(thisDate).format('DD MM YYYY'), onClick: date =>
+    onClick(date), currentDate: date, date: moment(previousMonth), startDate: startDate, endDate: endDate
+}));
+    }
+
+    for (let i = 1; i <= daysInMonth; i++) {
+        thisDate.date(i);
+
+        days.push(
+        React.createElement(Day, {key: moment(thisDate).format('DD MM YYYY'), onClick: date =>
+    onClick(date), currentDate: date, date: moment(thisDate), startDate: startDate, endDate: endDate}));
+    }
+
+    const daysCount = days.length;
+    for (let i = 1; i <= 42 - daysCount; i++) {
+        nextsMonth.date(i);
+        days.push(
+        React.createElement(Day, {key: moment(nextsMonth).format('DD MM YYYY'), onClick: date =>
+    onClick(date), currentDate: date, date: moment(nextsMonth), startDate: startDate, endDate: endDate }));
+    }
+
+    return(
+        React.createElement("nav", {className: "calendar--days"},
+        labels.concat(),
+        days.concat())
+    );
+};
+
+class Calendar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            date: moment(),
+            startDate: moment().subtract(5, 'day'),
+            endDate:moment().add(3, 'day')};
+    }
+
+    resetDate() {
+        this.setState({
+            date: moment() });
+    }
+
+    changeMonth(month) {
+        const {date} = this.state;
+
+        date.month(month);
+
+        this.setState(
+        date);
+    }
+
+    changeDate(date) {
+        let { startDate, endDate} = this.state;
+
+        if (startDate === null || date.isBefore(startDate, 'day') || !startDate.isSame(endDate, 'day')) {
+            startDate = moment(date);
+            endDate = moment(date);
+        } else if (date.isSame(startDate, 'day') && date.isSame(endDate, 'day')) {
+            startDate = null;
+            endDate = null;
+        } else if (date.isAfter(startDate, 'day')) {
+            endDate = moment(date);
+        }
+
+        this.setState({
+            startDate,
+            endDate});
+    }
+
+    render() {
+        const {date, startDate, endDate } = this.state;
+
+        return(
+            React.createElement("div", {className: "calendar"},
+            React.createElement(Heading, {date: date, changeMonth: month => this.changeMonth(month), resetDate: () => this.resetDate() }),
+            
+            React.createElement(Days, {onClick: date => this.changeDate(date), date: date, startDate: startDate, endDate: endDate})));
+    }}
+
+ReactDOM.render(
+React.createElement(Calendar, null),
+document.getElementById('calendar'));
